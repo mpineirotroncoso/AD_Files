0f6bce053a24d6fac856fc81916515eddb33a89d
mpineirotroncoso <145323703+mpineirotroncoso@users.noreply.github.com>
2024-09-16 10:46:07 +0200

    comprobar ficheros y directorios

--- src/File.java
+++ src/File.java
@@ -1,4 +1,8 @@
 public class File {
+    /**
+     * Comprueba si es un directorio
+     * @param path ruta del directorio a comprobar
+     */
     private void eDirectorio(String path)  {
         java.io.File file = new java.io.File(path);
         if (file.isDirectory()) {
@@ -6,6 +10,20 @@ public class File {
         }
         else {
             System.out.println("no es un directorio");
+        }
+    }
+
+    /**
+     * Comprueba si es un fichero
+     * @param path ruta del directorio a comprobar
+     */
+    private void eFicheiro(String path)  {
+        java.io.File file = new java.io.File(path);
+        if (file.isFile()) {
+            System.out.println("es un fichero");
+        }
+        else {
+            System.out.println("no es un fichero");
         }
     }
 }
